c8bf87612299e8e4306d81e1f782303a8b563980
Muhammad Zunnoorain Rafi <bhoshyl@gmail.com>
2023-12-09 22:54:36 +0500

    prisma setup

--- app/routes/expenses._index.tsx
+++ app/routes/expenses._index.tsx
@@ -1,42 +1,8 @@
-import { Link } from '@remix-run/react';
-import React from 'react';
-
 function Expenses() {
   return (
-    <div>
-      <div className="flex items-center justify-between pb-3 max-w-2xl border-b  mx-auto">
-        <h1 className="font-semibold">Create new expense</h1>
-        <div>
-          {/* Open the modal using document.getElementById('ID').showModal() method */}
-          <button
-            className="btn btn-sm"
-            onClick={() =>
-              (document.getElementById(
-                'my_modal_5'
-              ) as HTMLDialogElement)!.showModal()
-            }
-          >
-            + Add
-          </button>
-          <dialog
-            id="my_modal_5"
-            className="modal modal-bottom sm:modal-middle"
-          >
-            <div className="modal-box">
-              <h3 className="font-bold text-lg">Hello!</h3>
-              <p className="py-4">
-                Press ESC key or click the button below to close
-              </p>
-              <div className="modal-action">
-                <form method="dialog">
-                  <button className="btn">Close</button>
-                </form>
-              </div>
-            </div>
-          </dialog>
-        </div>
-      </div>
-    </div>
+   <div>
+    Expense index only data
+   </div>
   );
 }
 
